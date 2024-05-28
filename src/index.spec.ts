d6b1abb881e96d3e477d1a7c9923c7b726e94c61
Arnaud Valle <arnaud.valle@gmail.com>
2024-05-28 22:42:31 +0200

    test: add basic test suite

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,10 +1,18 @@
 import test from "ava";
+import { generateLorraineIpsum } from "./index";
 
-test("foo", (t) => {
-  t.pass();
+test("it returns 3 words separated by a space when asked for 3 words", (t) => {
+  const result = generateLorraineIpsum(3);
+  t.is(result.split(" ").length, 3);
 });
 
-test("bar", async (t) => {
-  const bar = Promise.resolve("bar");
-  t.is(await bar, "bar");
+test("fails when first parameter is smaller than 0", (t) => {
+  const error = t.throws(
+    () => {
+      generateLorraineIpsum(-1);
+    },
+    { instanceOf: TypeError },
+  );
+
+  t.is(error.message, "numberOfWords must be greater than 0");
 });
