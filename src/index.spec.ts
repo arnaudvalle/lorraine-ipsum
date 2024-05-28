654d9693e8e7051264c18e6e9c21cc03002b6d3f
Arnaud Valle <arnaud.valle@gmail.com>
2024-05-28 22:46:01 +0200

    refactor: move error check to macro
    Following the best practices from ava's documentation https://github.com/avajs/ava/blob/main/docs/recipes/typescript.md\#using-macros

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,18 +1,25 @@
 import test from "ava";
 import { generateLorraineIpsum } from "./index";
 
+const errorMacro = test.macro((t, input: number, expected: string) => {
+  const error = t.throws(
+    () => {
+      generateLorraineIpsum(input);
+    },
+    { instanceOf: TypeError },
+  );
+
+  t.is(error.message, expected);
+});
+
 test("it returns 3 words separated by a space when asked for 3 words", (t) => {
   const result = generateLorraineIpsum(3);
   t.is(result.split(" ").length, 3);
 });
 
-test("fails when first parameter is smaller than 0", (t) => {
-  const error = t.throws(
-    () => {
-      generateLorraineIpsum(-1);
-    },
-    { instanceOf: TypeError },
-  );
-
-  t.is(error.message, "numberOfWords must be greater than 0");
-});
+test(
+  "fails when first parameter is smaller than 0",
+  errorMacro,
+  -1,
+  "numberOfWords must be greater than 0",
+);
